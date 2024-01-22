aca37c1939dfe6220efa46e01d99b20739c49405
Matthew McClain <mthommcclain@gmail.com>
2024-01-22 11:19:15 -0800

    finish first pass at connect nodes

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
   import { reactive } from 'vue'
+  import { v4 as uuid } from 'uuid'
+
   const width = 700
   const height = 800
   const cardHeight = 150
   const cardWidth = 200
   const padding = 10
+  const tolerance = 30
 
   // construct nodes. we need them "raw" for event handling.
   // the positioning of nodes is a convention. For every 'from' 
@@ -18,7 +21,7 @@
   for (let i=0; i<fromChoices; i++) {
     nodes.push({
       type: 'from',
-      name: `from-node-${i}`,
+      id: uuid(),
       pos: {
         x: cardWidth,
         y: cardHeight/2 + i*(cardHeight + padding)
@@ -28,7 +31,7 @@
   for (let i=0; i<toChoices; i++) {
     nodes.push({
       type: 'to',
-      name: `to-node-${i}`,
+      id: uuid(),
       pos: {
         x: width - cardWidth,
         y: cardHeight/2 + i*(cardHeight + padding)
@@ -40,26 +43,64 @@
     fromChoices,
     toChoices,
     nodes,
+    workingStartNode: null,
     workingLine: null, // { to, from }
-    connections: [ [0,4], [2,5] ],
+    connections: [ ],
+    hoverNode: null
   })
 
+  function nodesConnectable(a,b) {
+    // connectable if different types and not already connected
+    if (a.type === b.type) return false
+    if (nodesConnected(a,b)) return false
+    return true
+  }
+
+  function nodesConnected(a,b) {
+    // x and y are ids, not nodes
+    return data.connections.some(([x,y]) => (a.id === x && b.id === y) || (a.id === y && b.id === x))
+  }
 
   function handleMousedown(e) {
     const pos = getSvgCoordinatesFromEvent(e)
-    const closestNode = getClosestNode(pos)
+    const node = getClosestNodeWithinTolerance(pos, tolerance)
+    if (!node) return
+    data.workingStartNode = node
     data.workingLine = {
-      from: closestNode.pos,
+      from: node.pos,
       to: pos
     }
   }
   function handleMouseup(e) {
+    if (!data.workingLine) return // ensure in "draw" mode
+
+    const pos = getSvgCoordinatesFromEvent(e)
+    const node = getClosestNodeWithinTolerance(pos, tolerance)
+    // if node and node is distinct from start, snap to it
+    if (node && nodesConnectable(node, data.workingStartNode)) {
+      data.connections.push([ node.id, data.workingStartNode.id ])
+    }
     data.workingLine = null
+    data.workingStartNode = null
   }
   function handleMousemove(e) {
+    const pos = getSvgCoordinatesFromEvent(e)
+    const node = getClosestNodeWithinTolerance(pos, tolerance)
+
     if (data.workingLine) {
-      data.workingLine.to = getSvgCoordinatesFromEvent(e)
+      if (node && data.workingStartNode && nodesConnectable(node, data.workingStartNode)) { // if there's a target node
+        data.workingLine.to = node.pos
+        data.hoverNode = node
+      }
+      else data.workingLine.to = pos
+    } else if (node) { // preview mode
+      // make node blue
+      data.hoverNode = node
     }
+  }
+
+  function getNodeById(id) {
+    return data.nodes.find(n => n.id === id)
   }
 
   function getClosestNode(pos, nodesArray = nodes) {
@@ -74,6 +115,12 @@
         return acc
       }
     }, nodesArray[0])
+  }
+
+  function getClosestNodeWithinTolerance(pos, tolerance, nodesArray = nodes) {
+    const closestNode = getClosestNode(pos, nodesArray)
+    const withinTolerance = closestNode && d(pos, closestNode.pos) <= tolerance
+    return withinTolerance ? closestNode : undefined
   }
 
   function d(pos1, pos2) {
@@ -105,7 +152,10 @@
 <template>
   <svg
     :viewBox="`0 0 ${width} ${height}`"
-    class="main-wrapper"
+    :class="{
+      'main-wrapper': true,
+      pointer: !!data.hoverNode
+    }"
     @mousemove="handleMousemove"
     @mousedown="handleMousedown"
     @mouseup="handleMouseup"
@@ -147,16 +197,17 @@
       :cx="n.pos.x"
       :cy="n.pos.y"
       :r="width/140"
+      :fill="data?.hoverNode?.id === n.id ? 'blue' : 'black' "
     />
 
     <!-- Fixed Connection -->
     <line
-      v-for="[from, to],i in data.connections"
+      v-for="[idFrom, idTo],i in data.connections"
       :key="`connection-${i}`"
-      :x1="nodes[from].pos.x"
-      :y1="nodes[from].pos.y"
-      :x2="nodes[to].pos.x"
-      :y2="nodes[to].pos.y"
+      :x1="getNodeById(idFrom).pos.x"
+      :y1="getNodeById(idFrom).pos.y"
+      :x2="getNodeById(idTo).pos.x"
+      :y2="getNodeById(idTo).pos.y"
       stroke="black"
       :stroke-width="width/220"
     />
@@ -184,5 +235,8 @@
 }
 svg {
   background: darkgrey;
+}
+svg.pointer:hover {
+  cursor: pointer;
 }
 </style>
