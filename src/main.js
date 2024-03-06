d70694cd275971cfe5e0aced8c6ef05c055f7395
Matthew McClain <mthommcclain@gmail.com>
2024-03-06 14:27:25 -0800

    rev agent

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import { browserAgent } from '@knowlearning/agents'
+import Agent from '@knowlearning/agents/browser.js'
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-window.Agent = browserAgent()
+window.Agent = Agent
 createApp(App).mount('#app')
