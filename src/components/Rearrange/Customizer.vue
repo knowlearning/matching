92d1f8e89107e26c038776bc633d27e1b23fe9aa
Matthew McClain <mthommcclain@gmail.com>
2024-03-18 10:00:01 -0700

    scaffold for rearrange type

---
+++ src/components/Rearrange/Customizer.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="rearrange-customizer">
+		Rearrange
+		<div
+			v-for="id in data.content.items"
+			:key="id"
+		>
+			<div>{{ id }}</div>
+			<button @click="rearrange(id,1)">Up</button>
+			<button @click="rearrange(id,-1)">Down</button>
+		</div>
+	</div>
+</template>
+
+
+<script setup>
+import { reactive } from 'vue'
+const props = defineProps(['id'])
+
+const data = reactive({
+  content: null
+})
+
+const state = await Agent.state(props.id)
+data.content = state
+
+function rearrange(id, n) {
+	const firstEl = data.content.items[0]
+	data.content.items.push(firstEl)
+
+}
+
+</script>
--- src/helpers/questionTypes.js
+++ src/helpers/questionTypes.js
@@ -2,6 +2,8 @@ import MatchingPlayer from '../components/Matching/Player.vue'
 import MatchingCustomizer from '../components/Matching/Customizer.vue'
 import TextEntryPlayer from '../components/TextEntry/Player.vue'
 import TextEntryCustomizer from '../components/TextEntry/Customizer.vue'
+import RearrangeCustomizer from '../components/Rearrange/Customizer.vue'
+
 
 export default {
 	'application/json;type=matching' : {
@@ -28,6 +30,25 @@ export default {
 			name: 'Unnamed Text Entry Item',
 			question: 'Your Question Here',
 			answer: ''
+		}
+	},
+
+	'application/json;type=rearrange-items' : {
+		description: 'Rearrange Items',
+		components: {
+			player: RearrangeCustomizer,
+			customizer: RearrangeCustomizer
+		},
+		newItemSchema: {
+			name: 'New Rearrange Item',
+			question: '?? Question Text ??',
+			items: [
+				'24f35050-e53f-11ee-8de6-f3934262868d',
+				'27e39270-e53f-11ee-8de6-f3934262868d',
+				'2aaaefd0-e53f-11ee-8de6-f3934262868d',
+				'2ded97b0-e53f-11ee-8de6-f3934262868d'
+			]
+
 		}
 	}
 }
