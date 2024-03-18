8ef958a7b591f0952cdbe449ed694d2eb0852f0b
Matthew McClain <mthommcclain@gmail.com>
2024-03-18 11:07:08 -0700

    reorder default items

--- src/components/Rearrange/Customizer.vue
+++ src/components/Rearrange/Customizer.vue
@@ -1,13 +1,33 @@
 <template>
 	<div class="rearrange-customizer">
-		Rearrange
+    <h3>Rearrange Column Customizer</h3>
+		<h4>Item Scope Id :: {{ id }} </h4>
+    <label for="item-name">Item Name:</label>
+    <textarea
+      id="item-name"
+      v-model="data.content.name"
+    />
 		<div
-			v-for="id in data.content.items"
+			v-for="(id,i) in data.content.items"
 			:key="id"
+			class="image-and-buttons"
 		>
-			<div>{{ id }}</div>
-			<button @click="rearrange(id,1)">Up</button>
-			<button @click="rearrange(id,-1)">Down</button>
+
+			<ImgById :id="id" class="choice" />
+			<div class="buttons-wrapper">
+				<button
+					:disabled="i === 0"
+					@click="moveArrayItem(data.content.items,i,-1)"
+				>
+					Up
+				</button>
+				<button
+					:disabled="i === data.content.items.length - 1"
+					@click="moveArrayItem(data.content.items, i,1)"
+				>
+					Down
+				</button>
+			</div>
 		</div>
 	</div>
 </template>
@@ -15,6 +35,8 @@
 
 <script setup>
 import { reactive } from 'vue'
+import ImgById from './ImgById.vue'
+
 const props = defineProps(['id'])
 
 const data = reactive({
@@ -24,10 +46,41 @@ const data = reactive({
 const state = await Agent.state(props.id)
 data.content = state
 
-function rearrange(id, n) {
-	const firstEl = data.content.items[0]
-	data.content.items.push(firstEl)
-
+function moveArrayItem(arr, i, delta) {
+    let nIndex = i + delta
+    
+    if (nIndex < 0) nIndex = 0
+    else if (nIndex >= arr.length) nIndex = arr.length - 1
+        
+    const item = arr.splice(i, 1)[0]
+    arr.splice(nIndex, 0, item)
 }
 
 </script>
+
+<style scoped>
+.rearrange-customizer {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+.image-and-buttons {
+	background: antiquewhite;
+	margin: 10px;
+	padding: 10px;
+	border-radius: 8px;
+
+	display: flex;
+	flex-direction: row;
+}
+.image-and-buttons .buttons-wrapper {
+	display: flex;
+	flex-direction: column;
+	align-items: right;
+	justify-content: center;
+	margin-left: 30px;
+}
+img.choice {
+	width: 200px;
+}
+</style>
