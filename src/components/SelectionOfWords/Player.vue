991844753d346c50f4a686f0ccc699e68bb1a739
Eren Kahraman <erenkahraman@Eren-MacBook-Air.local>
2024-03-28 22:49:46 +0100

    Selection of Words adding contetn

--- src/components/SelectionOfWords/Player.vue
+++ src/components/SelectionOfWords/Player.vue
@@ -1,59 +1,34 @@
 <template>
     <div class="player">
-        <div>
-            <h1>Item Configuration</h1>
-            {{ itemState }}
+    <h3 v-if="itemState.configuration">{{ itemState.configuration.instructions }}</h3>
+    <div>
+        <div class="volume-icon" @click="toggleAudioPlayback">
+            <i :class="audioPlaying ? 'fas fa-pause' : 'fas fa-volume-up'"/>
         </div>
-        <div v-if="itemState.configuration">
-            <h1>Player state</h1>
-            {{ playState }}
-            <h1>Left</h1>
-            <div
-              v-for="{ id } in leftImages"
-              :key="id"
-              @click="playState.persistentKlState.selected[id] = !playState.persistentKlState.selected[id]"
-            >
-                {{ id }}
-                <span v-if="playState.persistentKlState.selected[id]">Woo!</span>
+        <div class="image-row">
+            <div class="column-left">
+                <kl-image
+                    v-for="image in leftImages"
+                    :key="image.id"
+                    :id="image.id"
+                    :class="{ selected: playState.persistentKlState.selected[image.id] }"
+                    @click="toggleImageSelection(image.id)"
+                    class="images"
+                />
             </div>
-            <h1>RIGHT!</h1>
-            <div
-              v-for="{ id } in rightImages"
-              :key="id"
-              @click="playState.persistentKlState.selected[id] = !playState.persistentKlState.selected[id]"
-            >
-                {{ id }}
-                <span v-if="playState.persistentKlState.selected[id]">Woo!</span>
-            </div>
-        </div>
-        <div v-else>Loading...</div>
-        <!--
-        <h3 v-if="data.content.instructions">{{ data.content.instructions }}</h3>
-        <div>
-            <div class="volume-icon" @click="toggleAudioPlayback">
-                <i :class="audioPlaying ? 'fas fa-pause' : 'fas fa-volume-up'"/>
-            </div>
-            <div class="image-row">
-                <div class="column-left">
-                    <div v-for="(option, index) in imageData[0]" :key="index" class="image-and-buttons-left">
-                        <p style="color: black;">{{ index + 1 }}) <kl-image :id="option.id" /></p>
-                        <div class="option-circle" @click="isSelected(index, 0)">
-                            <i :class="{ 'fas fa-check-circle': isSelected(index, 0), 'far fa-circle': !isSelected(index, 0) }" style="font-size: 1.5em; color: black;"></i>
-                        </div>
-                    </div>
-                </div>
-                <div class="column-right">
-                    <div v-for="(option, index) in imageData[1]" :key="index" class="image-and-buttons-right">
-                        <p style="color: black;">{{ index + 1 }}) <kl-image :id="option.id" /></p>
-                        <div class="option-circle" @click="isSelected(index, 1)">
-                            <i :class="{ 'fas fa-check-circle': isSelected(index, 1), 'far fa-circle': !isSelected(index, 1) }" style="font-size: 1.5em; color: black;"></i>
-                        </div>
-                    </div>
-                </div>
+            <div class="column-right">
+                <kl-image
+                    v-for="image in rightImages"
+                    :key="image.id"
+                    :id="image.id"
+                    :class="{ selected: playState.persistentKlState.selected[image.id] }"
+                    @click="toggleImageSelection(image.id)"
+                    class="images"   
+                />
             </div>
         </div>
-        -->
     </div>
+</div>
 </template>
 
 <script setup>
@@ -92,6 +67,42 @@ Agent
 Agent
   .state(props.id)
   .then(state => itemState.configuration = state)
-
+  
 </script>
+
+<style scoped>
+.player {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.image-row {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+  object-fit: contain;
+}
+.column-left, .column-right {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+} 
+.images {
+    background: antiquewhite;
+    object-fit: contain;
+    padding: 20px;
+    border-radius: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    cursor: pointer;
+    margin: 15px;
+    object-fit: contain;
+}
+.volume-icon {
+  cursor: pointer;
+  font-size: 2em;
+  margin-bottom: 10px;
+}
+</style>
 
