178900f61d2645e968105a7238774a38a5850d54
Matthew McClain <mthommcclain@gmail.com>
2024-04-07 22:37:17 -0700

    add demo lang toggle

--- src/store/store.js
+++ src/store/store.js
@@ -4,30 +4,55 @@ import matchNavigatorLanguage from './matchNavigatorLanguage.js'
 
 const store = createStore({
   state: {
-  	language: matchNavigatorLanguage(),
+    language: matchNavigatorLanguage(),
     translations: {
-    	// TODO:  Maybe minimal isolate to file. Maybe maximal grab live data.
-    	'are-you-sure' : {
-    		'en' : 'Are you sure?',
-    		'th' : 'IN THAI are you sure?'
-    	},
-    	'cancel': {
-    		'en' : 'Cancel',
-    		'th' : 'IN THAI cancel'
-    	}
+      // TODO:  Maybe minimal isolate to file. Maybe maximal grab live data.
+      'are-you-sure' : {
+        'en' : 'Are you sure?',
+        'th' : 'คุณแน่ใจหรือไม่?'
+      },
+      'cancel': {
+        'en' : 'Cancel',
+        'th' : 'ยกเลิก'
+      },
+      'player': {
+        'en' : 'Player',
+        'th' : 'ผู้เล่น'
+      },
+      'customizer': {
+        'en' : 'Customizer',
+        'th' : 'ปรับแต่ง'
+      },
+      'add-new': {
+        'en' : 'Add New',
+        'th' : 'เพิ่มใหม่'
+      },
+      'copy-existing': {
+        'en' : 'Copy Existing',
+        'th' : 'คัดลอกที่มีอยู่'
+      },
+      'thai': {
+        'en' : 'Thai',
+        'th' : 'ไทย'
+      }
     },
   },
    getters: {
     language: state => () => state.language,
     t: state => slug => {
-    	const lang = state.language
-    	if (!state.translations[slug]) return `no slug ${slug}`
-    	else if (!state.translations[slug][lang]) return `no translation for ${slug} in ${lang}`
-     	else return state.translations[slug][lang]
+      const lang = state.language
+      if (!state.translations[slug]) return `no slug ${slug}`
+      else if (!state.translations[slug][lang]) return `no translation for ${slug} in ${lang}`
+       else return state.translations[slug][lang]
     },
   },
-  mutations: { },
-  actions: { },
+  mutations: {
+    language: (state, value) => state.language = value
+  },
+  actions: {
+    language: ({ commit }, value) => commit('language', value)
+    
+  },
 
 })
 
