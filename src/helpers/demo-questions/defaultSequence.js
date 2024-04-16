1cdb7d90477bd9f2b8e0ff19f73b059192255a4f
Matthew McClain <mthommcclain@gmail.com>
2024-04-15 18:49:43 -0700

    update default seq to items built in new domain

--- src/helpers/demo-questions/defaultSequence.js
+++ src/helpers/demo-questions/defaultSequence.js
@@ -1,8 +1,8 @@
 export default {
 	name: 'ลำดับใหม่ / New Sequence',
 	items: [
-		{ id : '4b0f0570-f8ec-11ee-8881-b3d50f9242a3' }, // Butterfly Reordering
-		{ id : '05f167b0-f6be-11ee-a444-c3b457a7db7e' }, // Kobe_MJ Match
-		{ id : '236450a0-f6be-11ee-a444-c3b457a7db7e' }, // Monument Pairs
+		{ id : '01fd0ef0-fb93-11ee-a57d-19b2f6cf4b58' }, // Butterfly Reordering
+		{ id : '0c014c40-fb93-11ee-a57d-19b2f6cf4b58' }, // Kobe_MJ Match
+		{ id : '13d0f650-fb93-11ee-a57d-19b2f6cf4b58' }, // Monument Pairs
 	]
 }
