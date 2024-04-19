1b7636c034ac6dbfb76ad17a159674a968ac4c5e
Matthew McClain <mthommcclain@gmail.com>
2024-04-19 15:22:00 -0700

    refactor play state and props

--- src/components/Sequence/SequenceFooter.vue
+++ src/components/Sequence/SequenceFooter.vue
@@ -20,7 +20,7 @@
 </template>
 
 <script setup>
-import {ref, computed } from 'vue'
+import { computed } from 'vue'
 import ProgressPill from './ProgressPill.vue'
 import DisplayTime from './DisplayTime.vue'
 
