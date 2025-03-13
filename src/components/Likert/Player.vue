40a67c1842cf6408f4854f93995b9ca31e6d6c96
Matthew McClain <mthommcclain@gmail.com>
2025-03-13 14:45:54 -0700

    add likert translations and clean up

--- src/components/Likert/Player.vue
+++ src/components/Likert/Player.vue
@@ -11,9 +11,7 @@
 			<v-row>
 				<v-col cols="1" />
 				<!-- one on each side, 5 of width 2 in the middle -->
-				<v-col cols="2"
-					v-for="(option, i) in radioOptions" :key="`label-${i}`"
-				>
+				<v-col cols="2" v-for="(option, i) in radioOptions" :key="`label-${i}`">
 					{{ option.label }}
 				</v-col>
 				<v-col cols="1" />
@@ -21,10 +19,7 @@
 			<v-row no-gutters>
 				<v-col cols="1" />
 				<!-- one on each side, 5 of width 2 in the middle -->
-				<v-col cols="2"
-					v-for="(option, i) in radioOptions" :key="`option-${i}`"
-
-				>
+				<v-col cols="2" v-for="(option, i) in radioOptions" :key="`option-${i}`">
 					<v-radio
 						v-model="selectedValue"
 						style="justify-content: center;"
@@ -37,18 +32,12 @@
 	</v-container>
 </template>
 
+
 <script setup>
 import likertCategories from './likertCategories.js'
 import { ref, computed } from 'vue'
 import { useStore } from 'vuex'
-
 function t(slug) { return useStore().getters.t(slug) }
-
-function handleSelection(e) {
-
-	console.log(e.target.value)
-}
-
 const props = defineProps({
 	id: {
 		type: String,
@@ -57,20 +46,13 @@ const props = defineProps({
 })
 
 const selectedValue = ref(null)
-
 const questionDef = await Agent.state(props.id)
-
 const radioOptions = computed(() => {
 	if (selectedValue === null) return null
 	const choices = likertCategories[questionDef.category]
-	console.log(choices)
 	return choices.map((el,i) => ({
 		label: t(el),
 		value: i + 1
 	}))
-
 })
-
-
-
 </script>
