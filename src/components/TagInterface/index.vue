7fea9c7e44da974ff82ef489507324a08f618af6
Jason Ford <fordjason@gmail.com>
2025-04-09 15:12:26 -0700

    Add tag interface

--- src/components/TagInterface/index.vue
+++ src/components/TagInterface/index.vue
@@ -4,87 +4,74 @@
 			Tags for
 			<ItemName :id="props.id" />
 		</h2>
-
-		<div v-if="displayState === 'loading'">
-			Loading
-		</div>
-
-		<div v-else-if="displayState === 'error'">
-			Error Setting Tag, Likely Insufficient Permissions
-		</div>
-
-		<div v-else-if="displayState ==='loaded'">
-			<ToggleItemTag
-				v-for="tag in tagsToTagItemsWith"
-				:key="`toggle-tag-${tag}-on-item-${props.id}`"
-				:item="props.id"
-				:tag="tag"
-				:partition="PUBLIC_TAGS_PARTITION"
-				:domain="TAGS_DOMAIN"
-				@addTagging="addTagging(tag, $event)"
-			/>
-		</div>
+		<TagFilters
+			v-model="selectedTags"
+			:partition="PILA_PARTITION"
+			:roots="[TAGS_TO_TAG_ITEMS_WITH]"
+			select-leaves-only
+			:LabelComponent="TagTranslation"
+		/>
 	</div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import ItemName from '../ItemName.vue'
-import ToggleItemTag from './ToggleItemTag.vue'
+import { Filters as TagFilters } from '@knowlearning/tags'
+
 const TAGS_DOMAIN = 'tags.knowlearning.systems'
 const TAGS_TO_TAG_ITEMS_WITH = "c025ade0-0cc9-11f0-973f-1565207095e0"
 const PILA_PARTITION = 'PILA'
 const PUBLIC_TAGS_PARTITION = 'Public Tags'
 
-
 const props = defineProps([ 'id' ])
-let displayState = ref('loaded')
-
-// populates the LIST of tags that items could possibly be tagged with
-// no need for reactivity
-const tagsToTagItemsWith = ( await Agent.query(
-    'taggings-for-tag',
-    [ PILA_PARTITION, TAGS_TO_TAG_ITEMS_WITH ],
-    TAGS_DOMAIN
-  ).catch(err => {
-    console.warn(err)
-    return []
-  })).map(obj => obj?.target)
 
 const tags = await Agent.state('tags')
 
+const tagsForId = ref(
+	await Agent.query(
+		'taggings-for-target',
+		[ PUBLIC_TAGS_PARTITION, props.id],
+		TAGS_DOMAIN
+	)
+	.then(taggings => taggings.map(({ tag }) => tag))
+	.catch(error => {
+		console.log('error getting tags for target', error)
+		return []
+	})
+)
+
+const selectedTags = computed({
+	get() {
+		return tagsForId.value
+	},
+	set(selectedTags) {
+		const { added, removed } = diffIds(tagsForId.value, selectedTags)
+
+		tagsForId.value.splice(0,tagsForId.value.length)
+		selectedTags.forEach(id => tagsForId.value.push(id))
+
+		added.forEach(id => addTagging(id, true))
+		removed.forEach(id => addTagging(id, null))
+	}
+})
+
+function diffIds(oldIds, newIds) {
+  const oldSet = new Set(oldIds)
+  const newSet = new Set(newIds)
+  return {
+    added: newIds.filter(id => !oldSet.has(id)),
+    removed: oldIds.filter(id => !newSet.has(id))
+  }
+}
+
 async function addTagging(tag, value) {
-	displayState.value = 'loading'
-	if (!tags[tag]) tags[tag] = {}	
+	if (!tags[tag]) tags[tag] = {}
+
 	tags[tag][props.id] = {
 		value,
 		partition: PUBLIC_TAGS_PARTITION
-	} 
-	let newVal
-	const startTime = Date.now()
-	while (newVal !== !!value) {
-		if (Date.now() - startTime > 5000) {
-			displayState.value = 'error'
-			return
-		}
-		await new Promise(r => setTimeout(r, 500))
-		newVal = await fetchIsTagged({
-			tag,
-			partition: PUBLIC_TAGS_PARTITION,
-			item: props.id,
-			domain: TAGS_DOMAIN
-		})
 	}
-	displayState.value = 'loaded'
-}	
-
-async function fetchIsTagged({ tag, partition, item, domain }) {
-	const x = await Agent.query(
-		'tagging-for-target',
-		[ partition, tag, item],
-		domain
-	)
-	return !!x.length
 }
 
 </script>
